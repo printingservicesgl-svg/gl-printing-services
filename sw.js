c7245909becbf73d147435ad5d0d8c132a80f2ed
printingservicesgl-svg <printingservicesgl@gmail.com>
2026-04-15 03:32:29 +0800

    update test

--- sw.js
+++ sw.js
@@ -1,21 +1,9 @@
-// sw.js
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
-
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
-        const options = {
+        self.registration.showNotification(event.data.title, {
             body: event.data.body,
-            icon: 'GL_Logo.png', // Siguraduhing tama ang filename ng logo mo
-            vibrate: [200, 100, 200],
-            requireInteraction: true,
-            data: { url: self.location.origin }
-        };
-        self.registration.showNotification(event.data.title, options);
+            icon: 'GL_Logo.png', // Siguraduhin na exist ang file na ito
+            vibrate: [200, 100, 200]
+        });
     }
-});
-
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    event.waitUntil(clients.openWindow('/'));
 });
