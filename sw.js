2c28123782c49a5560a96cc1bdbe7bb9cc79f4ca
printingservicesgl-svg <printingservicesgl@gmail.com>
2026-04-15 03:22:02 +0800

    Update all
    index
    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
+// sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
 
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'GL_Logo.png', // Iyong logo
-        badge: 'GL_Logo.png',
-        vibrate: [200, 100, 200],
-        data: { url: data.url }
-    };
-    event.waitUntil(
-        self.notificationPermission === 'granted' && 
-        self.registration.showNotification(data.title, options)
-    );
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+        const options = {
+            body: event.data.body,
+            icon: 'GL_Logo.png', // Siguraduhing tama ang filename ng logo mo
+            vibrate: [200, 100, 200],
+            requireInteraction: true,
+            data: { url: self.location.origin }
+        };
+        self.registration.showNotification(event.data.title, options);
+    }
 });
 
-// Idagdag mo ito sa sw.js mo
-self.addEventListener('notificationclick', function(event) {
+self.addEventListener('notificationclick', (event) => {
     event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://printingservicesgl-svg.github.io/gl-printing-services/')
-    );
+    event.waitUntil(clients.openWindow('/'));
 });
